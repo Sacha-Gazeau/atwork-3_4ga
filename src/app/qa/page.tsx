8180b2bebed5f4a48f232cdb5a243aea08e34089
sacha-gazeau <sacha.gazeau@student.arteveldehs.be>
2025-12-10 14:04:45 +0100

    feat: add PrismaClient initialization for database access

--- src/app/qa/page.tsx
+++ src/app/qa/page.tsx
@@ -1,10 +1,18 @@
+import prisma from "@/lib/client";
 
-export default function Question() {
+export default async function Question() {
+  const questions = await prisma.question.findMany();
+
   return (
     <div>
-      <h2>
-        QA
-      </h2>
+      <h2>Veelgestelde vragen</h2>
+      <ul>
+        {questions.map((q) => (
+          <li key={q.id}>
+            <p>{q.quest}</p>
+            <p>{q.answer}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
