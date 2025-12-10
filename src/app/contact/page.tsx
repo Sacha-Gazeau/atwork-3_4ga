28b09b1f1db55b6c53d0270a2c6d29675296352a
sacha-gazeau <sacha.gazeau@student.arteveldehs.be>
2025-12-10 16:23:21 +0100

    feat: implement contact form with user input fields

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
 export default function Contact() {
-  return <div>
-    <h2>Contact</h2>
-  </div>;
+  return (
+    <>
+      <h2>Contact form</h2>
+      <form className="request-form">
+        <label className="request-form__field">
+          <input type="text" name="firstName" placeholder="Voornaam" />
+        </label>
+        <label className="request-form__field">
+          <input type="text" name="lastName" placeholder="Achternaam" />
+        </label>
+        <label className="request-form__field">
+          <input type="email" name="email" placeholder="Email" />
+        </label>
+        <label className="request-form__field">
+          <textarea name="text" placeholder="Text"></textarea>
+        </label>
+        <button type="submit" className="request-form__submit">
+          Versturen
+        </button>
+      </form>
+    </>
+  );
 }
