240f3d722c60a4203d7de657074a6f95890c1384
sacha-gazeau <sacha.gazeau@student.arteveldehs.be>
2025-12-11 09:11:08 +0100

    feat: enhance contact form layout and styling; improve CSS variable precision

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,17 +5,19 @@ export default function Contact() {
     <>
       <h2>Contact form</h2>
       <form className="request-form">
-        <label className="request-form__field">
-          <input type="text" name="firstName" placeholder="Voornaam" />
-        </label>
-        <label className="request-form__field">
-          <input type="text" name="lastName" placeholder="Achternaam" />
-        </label>
+        <div className="request-form__duo">
+          <label className="request-form__field">
+            <input type="text" name="firstName" placeholder="Voornaam" />
+          </label>
+          <label className="request-form__field">
+            <input type="text" name="lastName" placeholder="Achternaam" />
+          </label>
+        </div>
         <label className="request-form__field">
           <input type="email" name="email" placeholder="Email" />
         </label>
         <label className="request-form__field">
-          <textarea name="text" placeholder="Text"></textarea>
+          <textarea name="text" placeholder="Text" rows={4}></textarea>
         </label>
         <button type="submit" className="request-form__submit">
           Versturen
